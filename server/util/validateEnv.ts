11a3d6a821ed6dd02351becd5a97d8763a2d3436
Adam Brown <ab185311@ncr.com>
2020-09-21 11:54:03 +0100

    change default port to 8080

--- server/util/validateEnv.ts
+++ server/util/validateEnv.ts
@@ -2,7 +2,7 @@ import {cleanEnv, port, str} from 'envalid';
 
 export default () => {
     return cleanEnv(process.env, {
-        PORT: port({default: 5000}),
+        PORT: port({default: 8080}),
         KEY_FILE: str(),
         PROJECT_ID: str()
     });
